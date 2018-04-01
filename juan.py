47352cb67fca575759e1738af7a4308b082052ce
Juan Ruiz de Bustillo <juanruizdebustillo@MBP.local>
2018-04-01 21:48:46 +0200

    new changes and completion

--- juan.py
+++ juan.py
@@ -9,7 +9,6 @@ import pandas as pd
 import webbrowser
 
 
-
 # define parser
 parser = OptionParser()
 # Input argumento
@@ -19,19 +18,28 @@ parser = OptionParser(usage=usage)
 parser.add_option("-f","--file", action="store_true",dest="SAVETOFILE",help="write report to a file, with name of the searched company")
 parser.add_option("-e","--export", action="store_true",dest="EXPORTCSV",help="export .json file to a .csv readable one. (only with -f flag)")
 parser.add_option("-a","--args", action="store_true",dest="ARGUMENTS",help="open a webbrowser to show relevant search arguments.")
+parser.add_option("-i","--install", action="store_true",dest="INSTALL",help="execute to install all dependencies")
 if len(sys.argv)==1:
     parser.print_help(sys.stderr)
     sys.exit(1)
+
 # ejecuta función de input
 (options, args) = parser.parse_args()
 
 # Se define el access token
 application = linkedin.LinkedInApplication(token='AQV48ylIhFTFtAazTsszjj6wIpx1c0B004m5hleCSaqlWPUxEXe7QVLxtzvUNnl_aAsX27ABK4j1uXLxcLnmb4M179xH06nSpDy7Eeahg9gdWLrhzjgB2sY2fQ4mU0Nk1Rs5KQ3FAaifqju9ry08WoCZk04sQ181WqsLXPm5JvHL0TY20THdvQff4KGA_GZznN-uyNNOlqDwMZ1HBm6UkuKARGN1CtjBaHuGgeVK0D2F2P38gjwG0KH_RO5uXFp_4sWFWggtNptY1zXHb1vHcFOOBSTQG_EjN58bjiHVPeMPYRuE1AyiWsFCqBxhKCMR-qCs40iH7hkBYeyXAv8gk7VFrR93aA')
 
+
+
 if options.ARGUMENTS==True:
 	webbrowser.open('https://developer.linkedin.com/docs/fields/company-profile', new=2)
 	print("Showing possible search arguments and quitting.")
 	quit()
+
+if options.INSTALL==True:
+	os.system("sudo python setup.py install")
+	quit()
+	
 
 # función de printeo de empresa
 def printCompanyInfo(company):

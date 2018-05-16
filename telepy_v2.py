fc05b786fd827fc3b781db9df0287d76a060ebe3
Juan Ruiz de Bustillo <juanruizdebustillo@MBP.local>
2018-05-16 10:44:46 +0200

    Added AR support

--- telepy_v2.py
+++ telepy_v2.py
@@ -524,14 +524,14 @@ try:
 except:
     pass
 
-TELEPY: TELEPY = TELEPY()
+TELEPY = TELEPY()
 
 TELEPY.drop_duplicates()
 
 df, rows = TELEPY.read_source_csv()
 
 
-df = TELEPY.doQuery(155, rows, 0, 3, 0)
+df = TELEPY.doQuery(1, rows, 0, 3, 0)
 
 print(bcolors.WARNING + '\nIt took', humanfriendly.format_timespan(time.time() - start), 'seconds to fetch',
       len(os.listdir("output/json/")), 'JSON files, from which',
